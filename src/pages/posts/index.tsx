a03984fb8c0d6dc341691798553769367a18a7f6
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2021-05-11 21:34:11 -0300

    list posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,10 +2,22 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import styles from './style.module.scss';
 
-export default function Posts() {
+type Post = {
+	slug: string;
+	title: string;
+	excerpt: string;
+	updatedAt: string;
+}
+
+interface PostsProps {
+	posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
 	return (
 		<>
 			<Head>
@@ -14,21 +26,13 @@ export default function Posts() {
 		
 			<main className={styles.container}>
 				<div className={styles.posts}>
-					<a href="#">
-						<time>12 de março de 2021</time>
-						<strong>Build your Responsive website without media query</strong>
-						<p>Since the introduction of media queries (before 2000), CSS has evolved and now (in 2021) there are a lot of tricks that can help you drastically reduce the usage of media queries and create an optimized code. I will even show you how to replace multiple media queries with only one CSS declaration.</p>
-					</a>
-					<a href="#">
-						<time>12 de março de 2021</time>
-						<strong>Build your Responsive website without media query</strong>
-						<p>Since the introduction of media queries (before 2000), CSS has evolved and now (in 2021) there are a lot of tricks that can help you drastically reduce the usage of media queries and create an optimized code. I will even show you how to replace multiple media queries with only one CSS declaration.</p>
-					</a>
-					<a href="#">
-						<time>12 de março de 2021</time>
-						<strong>Build your Responsive website without media query</strong>
-						<p>Since the introduction of media queries (before 2000), CSS has evolved and now (in 2021) there are a lot of tricks that can help you drastically reduce the usage of media queries and create an optimized code. I will even show you how to replace multiple media queries with only one CSS declaration.</p>
-					</a>
+					{ posts.map(post => (
+						<a key={post.slug} href="#">
+							<time>{post.updatedAt}</time>
+							<strong>{post.title}</strong>
+							<p>{post.excerpt}</p>
+						</a>
+					))}
 				</div>
 			</main>
 		</>
@@ -45,9 +49,20 @@ export const getStaticProps: GetStaticProps = async () => {
 		pageSize: 100,
 	})
 
-	console.log(JSON.stringify(response, null, 2))
+	const posts = response.results.map(post => {
+		return {
+			slug: post.uid,
+			title: RichText.asText(post.data.title),
+			excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+			updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+				day: '2-digit',
+				month: 'long',
+				year: 'numeric'
+			})
+		};
+	});
 
 	return {
-		props: {}
+		props: { posts }
 	}
 }
