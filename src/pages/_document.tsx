a16464a84b48704f00b1e97d7a9f889612082ae8
Rodrigo Bunhak <bunhak.rodrigo@gruposervices.com.br>
2022-06-20 00:12:59 -0300

    Title dinâmico por página

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;0,900;1,400&display=swap" rel="stylesheet" />
-                    <title>ig.news</title>
                 </Head>
                 <body>
                     <Main />
