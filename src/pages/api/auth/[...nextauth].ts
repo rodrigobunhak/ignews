58aa56d016afcb7691ad9d33a8d2709e6a2f2cee
Rodrigo Bunhak <bunhak.rodrigo@gruposervices.com.br>
2022-06-22 15:18:33 -0300

    Salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,5 +19,23 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  secret: process.env.NEXTAUTH_URL
+  secret: process.env.NEXTAUTH_URL,
+  callbacks: {
+    async signIn({user, account, profile}) {
+      const { email } = user
+      
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        )
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    }
+  }
 })
