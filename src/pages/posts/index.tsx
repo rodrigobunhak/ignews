49a5655d476c09cfc5d75d57d3d46e270dba9eab
Rodrigo Bunhak <bunhak.rodrigo@gruposervices.com.br>
2022-06-23 20:28:03 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,9 +2,20 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
-import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+}
+
+interface PostsProps {
+    posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -13,24 +24,13 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a>
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with bla bla bla</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi aliquid enim quis error, laudantium minus porro. Inventore, exercitationem quisquam. Qui iste temporibus placeat sunt voluptatem dolor cupiditate quisquam laboriosam eum.</p>
-                    </a>
-
-                    <a>
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with bla bla bla</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi aliquid enim quis error, laudantium minus porro. Inventore, exercitationem quisquam. Qui iste temporibus placeat sunt voluptatem dolor cupiditate quisquam laboriosam eum.</p>
-                    </a>
-
-                    <a>
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with bla bla bla</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi aliquid enim quis error, laudantium minus porro. Inventore, exercitationem quisquam. Qui iste temporibus placeat sunt voluptatem dolor cupiditate quisquam laboriosam eum.</p>
-                        
-                    </a>
+                    { posts.map(post => (
+                        <a key={post.slug} href="#">
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    ))}
                 </div>
             </main>
         </>
@@ -44,9 +44,20 @@ export const getStaticProps: GetStaticProps = async () => {
         pageSize: 100,
     })
 
-    console.log(response)
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
 
     return {
-        props: {}
+        props: {posts}
     }
 }
